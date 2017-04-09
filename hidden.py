fde9dd8d579913598da837402dbda455d0ba6311
anish12341 <anishpatel345@gmail.com>
2017-04-09 21:05:23 +0530

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -1,6 +1,7 @@
 def oauth():
     return {
-	    "consumer_key":"Siqb9VHqhB0NWlRGGw5zjOQyv",
-		"consumer_secret":"fiRdlamtkLZj3mGh3i0bOcyyGRCC4HgCXWesAF43Dp7PQJuiry",
-		"token_key":"2545708711-pQXxy9Xa5QARmTa7eLLiKP3fKN8UBQbD1E9GF01",
-		"token_secret":"oAQFmPYZOCTgPOMmYAgQJqe8CXp30UZYanm2UfF63j0mD"}
+	    "consumer_key":"zdhdjhdkal....",
+		"consumer_secret":"sdjcksdjkdskdjkj..",
+		"token_key":"nsmncmsnmcsdnkl......",
+		"token_secret":"lksckajckajd.."}
+        #GET THESE KEYS OF YOUR OWN FROM https://apps.twitter.com/ BY REGISTERING YOUR OWN APPLICATION
